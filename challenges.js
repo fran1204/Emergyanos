7314fd0ca38f061fb6f1869958da23cfbe770131
Pepe Garcia <pgarciag93@gmail.com>
2017-07-01 00:59:19 +0200

    initial commit

---
+++ challenges.js
@@ -0,0 +1,194 @@
+var challenges = [
+  {
+    "day": 1,
+    "challenge": "Busca a un compañero de otra tecnología y dale un beso (si es en la boca tiene más puntos)"
+  },
+  {
+    "day": 2,
+    "challenge": "Interrumpe un daily o reunión y échate una foto. (Si es de otro equipo, sea o no de Emergya -BPO-, puntos extra)"
+  },
+  {
+    "day": 3,
+    "challenge": "Atrévete a echarle un piropo a un compañero/a de otro equipo (si estás fuera, puede ser por hangout; y si es un responsable, puntos extra)"
+  },
+  {
+    "day": 4,
+    "challenge": "Disfruta de algún deporte con alguien del trabajo"
+  },
+  {
+    "day": 5,
+    "challenge": "Ponle a alguien un fiso en el ratón"
+  },
+  {
+    "day": 6,
+    "challenge": "Hazte una foto divertida con tu equipo y tuiteala (o FB) mencionando a Emergya"
+  },
+  {
+    "day": 7,
+    "challenge": "Cambia la B por la V en el teclado"
+  },
+  {
+    "day": 8,
+    "challenge": "Instala la extensión de Chrome NCage en el pc de un compañero https://chrome.google.com/webstore/detail/ncage/hnbmfljfohghaepamnfokgggaejlmfol?hl=es"
+  },
+  {
+    "day": 9,
+    "challenge": "Haz un montaje con parecidos razonables de compañeros y famosos"
+  },
+  {
+    "day": 10,
+    "challenge": "Haz RT al tweet de Emergya"
+  },
+  {
+    "day": 11,
+    "challenge": "Cambia el idioma del Gmail de un compañero a vasco"
+  },
+  {
+    "day": 12,
+    "challenge": "Comparte alguna información útil con tus compañeros"
+  },
+  {
+    "day": 13,
+    "challenge": "Cámbiate el nombre del hangout"
+  },
+  {
+    "day": 14,
+    "challenge": "Divierte y aprende SEO con la presentación de Bea  https://docs.google.com/presentation/d/1YTnIO6hxZIStd1B62uR4-cze41nhnsKFKhlRcgCRhLs/edit?usp=gmail"
+  },
+  {
+    "day": 15,
+    "challenge": "Aprende algo útil sobre otra tecnología y aplícalo en tu proyecto"
+  },
+  {
+    "day": 16,
+    "challenge": "Busca un vídeo en Youtube de alguno de tus compañeros"
+  },
+  {
+    "day": 17,
+    "challenge": "Busca al compañero más alto y más bajo de la oficina y hazles una foto juntos"
+  },
+  {
+    "day": 18,
+    "challenge": "Sustituye tu bebida energética favorita por una tila"
+  },
+  {
+    "day": 19,
+    "challenge": "Secuestra un muñeco de un compañero y pide un rescate de chuches"
+  },
+  {
+    "day": 20,
+    "challenge": "Piensa en un proyecto innnovador y compártelo con el departamento de I+D (mgimenez@emergya.com)"
+  },
+  {
+    "day": 21,
+    "challenge": "Haz un boomerang gracioso con tus compañeros"
+  },
+  {
+    "day": 22,
+    "challenge": "Utiliza A TOPE o BACAN muchas veces en un correo"
+  },
+  {
+    "day": 23,
+    "challenge": "Da un abrazo a un compañero de otro equipo de otra nacionalidad"
+  },
+  {
+    "day": 24,
+    "challenge": "Cámbiate la foto del hangout"
+  },
+  {
+    "day": 25,
+    "challenge": "Aprende algo nuevo sobre un compañero y... ¡demuéstralo!"
+  },
+  {
+    "day": 26,
+    "challenge": "Haz correr el rumor de que es el cumpleaños de un compañero y convence al resto para que le compren una tarta"
+  },
+  {
+    "day": 27,
+    "challenge": "Regala una chocolatina a alguien que tenga un mal día"
+  },
+  {
+    "day": 28,
+    "challenge": "Quítale las gafas a un compañero."
+  },
+  {
+    "day": 29,
+    "challenge": "Escóndele el casco a un compañero motero"
+  },
+  {
+    "day": 30,
+    "challenge": "Ponte de fondo para trabajar canciones de Disney"
+  },
+  {
+    "day": 31,
+    "challenge": "Envía un mail a comunicacion@emergya.com alabando el trabajo de un compañero"
+  },
+  {
+    "day": 32,
+    "challenge": "Ponle un post-it amoroso a un compañero."
+  },
+  {
+    "day": 33,
+    "challenge": "Ponte alguna prenda del revés"
+  },
+  {
+    "day": 34,
+    "challenge": "Cambia el fondo de pantalla de un compañero despistado."
+  },
+  {
+    "day": 35,
+    "challenge": "Trae chuches/dulces/chocolates para tu equipo"
+  },
+  {
+    "day": 36,
+    "challenge": "Ponle alguna chincheta en la silla a un compañero"
+  },
+  {
+    "day": 37,
+    "challenge": "Dale las gracias a un compañero por algo que te haya enseñado"
+  },
+  {
+    "day": 38,
+    "challenge": "Finge que es tu cumpleaños y que has dejado una tarta en la cocina."
+  },
+  {
+    "day": 39,
+    "challenge": "Decora tu zona de trabajo y la de tus compañeros con mensajes positivos y motivadores"
+  },
+  {
+    "day": 40,
+    "challenge": "¡Te mereces un capricho! Hoy haz tu desayuno favorito"
+  },
+  {
+    "day": 41,
+    "challenge": "Hacemos equipo vistiendo alguna prenda del color corporativo (rojo emergya)"
+  },
+  {
+    "day": 42,
+    "challenge": "Poner la pantalla con windows loader"
+  },
+  {
+    "day": 43,
+    "challenge": "Modificar la salida de audio del ordenador, para que siempre salga por los altavoces."
+  },
+  {
+    "day": 44,
+    "challenge": "Invertir la rotación del monitor."
+  },
+  {
+    "day": 45,
+    "challenge": "Tómate una cerveza al salir del trabajo con tus compañeros... ¡y demuéstralo con foto!"
+  },
+  {
+    "day": 46,
+    "challenge": "Convoca a alguien a una reunión y cancelala justo antes de comenzar"
+  },
+  {
+    "day": 47,
+    "challenge": "Desenchúfale los cascos a un compañero mientras escucha música"
+  },
+  {
+    "day": 48,
+    "challenge": "Échale un cable a un compañero"
+  }
+]
